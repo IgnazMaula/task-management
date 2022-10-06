c76cac6ac6f56862555b50204c3a2fa3f9b2ff65
Ignaz Maula <ignazmaula@gmail.com>
2022-10-06 21:01:28 +0800

    MUI1-7 Mockup data

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,19 @@
-import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import axios from 'axios';
+import MockAdapter from 'axios-mock-adapter';
+import { TaskData } from './data';
 
 import './App.css';
 import BlankPage from './Page/BlankPage';
 import TaskPage from './Page/TaskPage';
 
 function App() {
+    // Api Mock
+    var mock = new MockAdapter(axios);
+    mock.onGet('/api/tasks').reply(200, {
+        TaskData,
+    });
+
     return (
         <Router>
             <Routes>
--- src/Page/TaskPage.tsx
+++ src/Page/TaskPage.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
-
+import { useState, useEffect } from 'react';
+import axios from 'axios';
 import { createTheme, ThemeProvider } from '@mui/material/styles';
 import CssBaseline from '@mui/material/CssBaseline';
 import Box from '@mui/material/Box';
@@ -11,10 +12,35 @@ import Paper from '@mui/material/Paper';
 import Task from '../components/Task';
 import Sidebar from '../components/Sidebar';
 
+import { TaskProps } from '../interface';
+import { NavLink } from 'react-router-dom';
+
 const mdTheme = createTheme();
 
 function TaskPage() {
     const [open, setOpen] = React.useState(true);
+    const [tasks, setTasks] = useState<TaskProps[]>([]);
+    const [completedTasks, setCompletedTasks] = useState<TaskProps[]>([]);
+    const [canceledTasks, setCanceledTasks] = useState<TaskProps[]>([]);
+    const [error, setError] = useState(false);
+
+    useEffect(() => {
+        const getData = async () => {
+            try {
+                const response = await axios('/api/tasks');
+                if (response.status !== 200) {
+                    throw new Error(`This is an HTTP error: The status is ${response.status}`);
+                }
+                setTasks(response.data.TaskData.Tasks);
+                setCompletedTasks(response.data.TaskData.CompletedTasks);
+                setCanceledTasks(response.data.TaskData.CanceledTasks);
+            } catch (error: any) {
+                setError(true);
+            }
+        };
+        getData();
+    }, []);
+
     const toggleDrawer = () => {
         setOpen(!open);
     };
@@ -34,73 +60,86 @@ function TaskPage() {
                     }}
                 >
                     <Container maxWidth='lg' sx={{ mt: 4, mb: 4 }}>
-                        <Grid container spacing={3}>
-                            <Grid item xs={12} md={4} lg={4}>
-                                <Paper
-                                    variant='outlined'
-                                    sx={{
-                                        p: 2,
-                                        display: 'flex',
-                                        flexDirection: 'column',
-                                        height: 865,
-                                    }}
-                                >
-                                    <Typography variant='h5' sx={{ fontWeight: 'bold', paddingTop: 1 }}>
-                                        Tasks
-                                    </Typography>
-                                    <Divider sx={{ my: 2 }} />
-                                    <Task title='Fresh Desk' closeDate='May 22, 2022' name='Lindsey Stroud' status='Urgent' />
-                                    <Task title='Skype' closeDate='May 28, 2022' name='George Fields' status='Default' />
-                                    <Typography align='center' sx={{ fontWeight: 'bold', paddingTop: 2 }} color='#9E9E9E'>
-                                        CREATE NEW
-                                    </Typography>
-                                </Paper>
+                        {error ? (
+                            <Typography color='error' align='center'>
+                                Error - failed to retrieve task data
+                            </Typography>
+                        ) : (
+                            <Grid container spacing={3}>
+                                <Grid item xs={12} md={4} lg={4}>
+                                    <Paper
+                                        variant='outlined'
+                                        sx={{
+                                            p: 2,
+                                            display: 'flex',
+                                            flexDirection: 'column',
+                                            height: 865,
+                                        }}
+                                    >
+                                        <Typography variant='h5' sx={{ fontWeight: 'bold', paddingTop: 1 }}>
+                                            Tasks
+                                        </Typography>
+                                        <Divider sx={{ my: 2 }} />
+                                        {tasks.map((t) => (
+                                            <Task key={t.title} title={t.title} closeDate={t.closeDate} name={t.name} status={t.status} />
+                                        ))}
+                                        <NavLink to='/newtask'>
+                                            <Typography align='center' sx={{ fontWeight: 'bold', paddingTop: 2 }} color='#9E9E9E'>
+                                                CREATE NEW
+                                            </Typography>
+                                        </NavLink>
+                                    </Paper>
+                                </Grid>
+                                <Grid item xs={12} md={4} lg={4}>
+                                    <Paper
+                                        variant='outlined'
+                                        sx={{
+                                            p: 2,
+                                            display: 'flex',
+                                            flexDirection: 'column',
+                                            height: 865,
+                                        }}
+                                    >
+                                        <Typography variant='h5' sx={{ fontWeight: 'bold', paddingTop: 1 }}>
+                                            Completed Tasks
+                                        </Typography>
+                                        <Divider sx={{ my: 2 }} />
+                                        {completedTasks.map((t) => (
+                                            <Task key={t.title} title={t.title} closeDate={t.closeDate} name={t.name} status={t.status} />
+                                        ))}
+                                        <NavLink to='/newtask'>
+                                            <Typography align='center' sx={{ fontWeight: 'bold', paddingTop: 2 }} color='#9E9E9E'>
+                                                CREATE NEW
+                                            </Typography>
+                                        </NavLink>
+                                    </Paper>
+                                </Grid>
+                                <Grid item xs={12} md={4} lg={4}>
+                                    <Paper
+                                        variant='outlined'
+                                        sx={{
+                                            p: 2,
+                                            display: 'flex',
+                                            flexDirection: 'column',
+                                            height: 865,
+                                        }}
+                                    >
+                                        <Typography variant='h5' sx={{ fontWeight: 'bold', paddingTop: 1 }}>
+                                            Canceled Tasks
+                                        </Typography>
+                                        <Divider sx={{ my: 2 }} />
+                                        {canceledTasks.map((t) => (
+                                            <Task key={t.title} title={t.title} closeDate={t.closeDate} name={t.name} status={t.status} />
+                                        ))}
+                                        <NavLink to='/newtask'>
+                                            <Typography align='center' sx={{ fontWeight: 'bold', paddingTop: 2 }} color='#9E9E9E'>
+                                                CREATE NEW
+                                            </Typography>
+                                        </NavLink>
+                                    </Paper>
+                                </Grid>
                             </Grid>
-                            <Grid item xs={12} md={4} lg={4}>
-                                <Paper
-                                    variant='outlined'
-                                    sx={{
-                                        p: 2,
-                                        display: 'flex',
-                                        flexDirection: 'column',
-                                        height: 865,
-                                    }}
-                                >
-                                    <Typography variant='h5' sx={{ fontWeight: 'bold', paddingTop: 1 }}>
-                                        Completed Tasks
-                                    </Typography>
-                                    <Divider sx={{ my: 2 }} />
-                                    <Task title='Netflix' closeDate='April 12, 2022' name='Rebecca Moore' status='Completed' />
-                                    <Task title='Instagram' closeDate='April 11, 2022' name='Nicci Troiani' status='Paid' />
-                                    <Task title='Google' closeDate='April 7, 2022' name='Jones Dermot' status='Paid' />
-                                    <Task title='Clockify' closeDate='April 5, 2022' name='Jane Doe' status='Completed' />
-                                    <Task title='Slack' closeDate='April 2, 2022' name='Franz Ferdinand' status='Completed' />
-                                    <Typography align='center' sx={{ fontWeight: 'bold', paddingTop: 2 }} color='#9E9E9E'>
-                                        CREATE NEW
-                                    </Typography>
-                                </Paper>
-                            </Grid>
-                            <Grid item xs={12} md={4} lg={4}>
-                                <Paper
-                                    variant='outlined'
-                                    sx={{
-                                        p: 2,
-                                        display: 'flex',
-                                        flexDirection: 'column',
-                                        height: 865,
-                                    }}
-                                >
-                                    <Typography variant='h5' sx={{ fontWeight: 'bold', paddingTop: 1 }}>
-                                        Canceled Tasks
-                                    </Typography>
-                                    <Divider sx={{ my: 2 }} />
-                                    <Task title='BBC' closeDate='April 2, 2022' name='Nikol Ricci' status='Canceled' />
-                                    <Typography align='center' sx={{ fontWeight: 'bold', paddingTop: 2 }} color='#9E9E9E'>
-                                        CREATE NEW
-                                    </Typography>
-                                </Paper>
-                            </Grid>
-                        </Grid>
+                        )}
                     </Container>
                 </Box>
             </Box>
