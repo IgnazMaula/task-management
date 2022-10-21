875f759dfa9614b5603a3938d3a0729301b3afa9
Ignaz Maula <ignazmaula@gmail.com>
2022-10-21 18:19:47 +0800

    MUI1-16 Add datepicker to create task

--- src/components/organisms/NewTaskModal/NewTaskModal.tsx
+++ src/components/organisms/NewTaskModal/NewTaskModal.tsx
@@ -7,6 +7,10 @@ import MenuItem from '@mui/material/MenuItem';
 import Modal from '@mui/material/Modal';
 import TextField from '@mui/material/TextField';
 import Typography from '@mui/material/Typography';
+import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
+import { DesktopDatePicker } from '@mui/x-date-pickers/DesktopDatePicker';
+import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
+import dayjs, { Dayjs } from 'dayjs';
 
 import { assignees, taskTypes } from '../../../data';
 import { NewTaskModelProps } from '../../../interface';
@@ -17,6 +21,7 @@ const NewTaskModal = ({ open, handleClose, handleSubmit }: NewTaskModelProps) =>
     const [description, setDescription] = useState('');
     const [taskType, setTaskType] = useState('Task');
     const [assignee, setAssignee] = useState('Lindsey Stroud');
+    const [date, setDate] = React.useState<Dayjs | null>(dayjs(Date.now()));
 
     const handleTaskType = (event: ChangeEvent<HTMLInputElement>) => {
         setTaskType(event.target.value);
@@ -25,83 +30,97 @@ const NewTaskModal = ({ open, handleClose, handleSubmit }: NewTaskModelProps) =>
         setAssignee(event.target.value);
     };
 
+    const handleChange = (newDate: Dayjs | null) => {
+        setDate(newDate);
+    };
+
     return (
-        <Modal
-            open={open}
-            onClose={handleClose}
-            aria-labelledby="modal-modal-title"
-            aria-describedby="modal-modal-description"
-        >
-            <form
-                onSubmit={(e) =>
-                    handleSubmit(e, {
-                        title: title,
-                        closeDate: 'May 22, 2022',
-                        name: assignee,
-                        status: 'Urgent',
-                        taskType: taskType,
-                    })
-                }
+        <LocalizationProvider dateAdapter={AdapterDayjs}>
+            <Modal
+                open={open}
+                onClose={handleClose}
+                aria-labelledby="modal-modal-title"
+                aria-describedby="modal-modal-description"
             >
-                <Box className={styles.container}>
-                    <Typography id="modal-modal-title" variant="h6" component="h2">
-                        Create New Task
-                    </Typography>
-                    <TextField
-                        className={styles.textField}
-                        id="filled-select-taskType"
-                        select
-                        label="Fill"
-                        value={taskType}
-                        onChange={handleTaskType}
-                        variant="filled"
-                    >
-                        {taskTypes.map((option) => (
-                            <MenuItem key={option.value} value={option.value}>
-                                {option.name}
-                            </MenuItem>
-                        ))}
-                    </TextField>
-                    <TextField
-                        className={styles.textField}
-                        label="Task Name"
-                        variant="filled"
-                        value={title}
-                        onChange={(e) => setTitle(e.target.value)}
-                    />
-                    <TextField
-                        className={styles.textField}
-                        label="Description"
-                        variant="filled"
-                        value={description}
-                        onChange={(e) => setDescription(e.target.value)}
-                    />
-                    <TextField
-                        className={styles.textField}
-                        id="filled-select-taskType"
-                        select
-                        label="Assigned to"
-                        value={assignee}
-                        onChange={handleAssignee}
-                        variant="filled"
-                    >
-                        {assignees.map((option) => (
-                            <MenuItem key={option.value} value={option.value}>
-                                {option.value}
-                            </MenuItem>
-                        ))}
-                    </TextField>
-                    <div className={styles.buttonGroup}>
-                        <Button variant="outlined" onClick={handleClose}>
-                            Cancel
-                        </Button>
-                        <Button variant="contained" type="submit">
-                            Create
-                        </Button>
-                    </div>
-                </Box>
-            </form>
-        </Modal>
+                <form
+                    onSubmit={(e) =>
+                        handleSubmit(e, {
+                            title: title,
+                            closeDate: date?.format('LL').toString() || Date.now().toString(),
+                            name: assignee,
+                            status: 'Urgent',
+                            taskType: taskType,
+                        })
+                    }
+                >
+                    <Box className={styles.container}>
+                        <Typography id="modal-modal-title" variant="h6" component="h2">
+                            Create New Task
+                        </Typography>
+                        <TextField
+                            className={styles.textField}
+                            id="filled-select-taskType"
+                            select
+                            label="Fill"
+                            value={taskType}
+                            onChange={handleTaskType}
+                            variant="filled"
+                        >
+                            {taskTypes.map((option) => (
+                                <MenuItem key={option.value} value={option.value}>
+                                    {option.name}
+                                </MenuItem>
+                            ))}
+                        </TextField>
+                        <TextField
+                            className={styles.textField}
+                            label="Task Name"
+                            variant="filled"
+                            value={title}
+                            onChange={(e) => setTitle(e.target.value)}
+                        />
+                        <TextField
+                            className={styles.textField}
+                            label="Description"
+                            variant="filled"
+                            value={description}
+                            onChange={(e) => setDescription(e.target.value)}
+                        />
+                        <TextField
+                            className={styles.textField}
+                            id="filled-select-taskType"
+                            select
+                            label="Assigned to"
+                            value={assignee}
+                            onChange={handleAssignee}
+                            variant="filled"
+                        >
+                            {assignees.map((option) => (
+                                <MenuItem key={option.value} value={option.value}>
+                                    {option.value}
+                                </MenuItem>
+                            ))}
+                        </TextField>
+                        <DesktopDatePicker
+                            className={styles.textField}
+                            label="Close date"
+                            inputFormat="MM/DD/YYYY"
+                            value={date}
+                            onChange={handleChange}
+                            renderInput={(params) => <TextField {...params} />}
+                        />
+                        <div className={styles.buttonGroup}>
+                            <Button variant="outlined" onClick={handleClose}>
+                                Cancel
+                            </Button>
+                            <Button variant="contained" type="submit">
+                                Create
+                            </Button>
+                        </div>
+                    </Box>
+                </form>
+            </Modal>
+        </LocalizationProvider>
     );
 };
 
